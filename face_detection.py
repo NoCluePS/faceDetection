18ed7b72937fe6261fc6a47f1f14333809ab4f35
PijusSerapinasMipsas <pijus.s@mipsas.com>
2021-12-05 13:47:07 +0200

    Added URL face detection

---
+++ face_detection.py
@@ -0,0 +1,36 @@
+import cv2
+import urllib.request
+import os
+import validators
+
+# Load data into py file
+trained_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+
+real_time = input('Real-time? (y/n) ') == 'y';
+
+if (not real_time):
+    url = input('Image url: ');
+    save_name = 'heads/head.jpg';
+
+    img = '';
+
+    while (not validators.url(url)):
+        print('Invalid URL')
+        url = input('Image URL: ')
+
+    urllib.request.urlretrieve(url, save_name);
+    img = cv2.imread(save_name);
+
+    grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_data.detectMultiScale(grayscaled_img);
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.putText(img, 'Face', (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow('Face detection', img);
+    cv2.waitKey();
+
+    os.remove('heads/head.jpg');
+else:
+    print('TBD, try out the image url part of the program')
